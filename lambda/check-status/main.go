aad3ac837bf4b26659af2d405da001c2e44acdbc
Karol Koś <kosiyyu@gmail.com>
2025-03-10 00:51:21 +0100

    Update check-status

--- lambda/check-status/main.go
+++ lambda/check-status/main.go
@@ -1,4 +1,4 @@
-package checkstatus
+package main
 
 import (
 	"context"
@@ -15,15 +15,15 @@ type Response struct {
 	Body       string `json:"body"`
 }
 
-func handleRequest(ctx context.Context, _ json.RawMessage) Response {
-	cluster := "your-cluster-name"
-	service := "your-service-name"
+func handleRequest(ctx context.Context, _ json.RawMessage) (Response, error) {
+	cluster := "minecraft-cluster"
+	service := "mincraft-server-service"
 
 	cfg, err := config.LoadDefaultConfig(ctx,
 		config.WithRegion("eu-north-1"),
 	)
 	if err != nil {
-		return Response{StatusCode: 500, Body: "Failed to load configurationt"}
+		return Response{StatusCode: 500, Body: "Failed to load configuration"}, err
 	}
 
 	ecsClient := ecs.NewFromConfig(cfg)
@@ -36,35 +36,23 @@ func handleRequest(ctx context.Context, _ json.RawMessage) Response {
 	result, err := ecsClient.DescribeServices(ctx, input)
 	if err != nil {
 		log.Printf("Failed to describe service: %v", err)
-		return Response{StatusCode: 500, Body: "Failed to check container status: " + err.Error()}
+		return Response{StatusCode: 500, Body: "Failed to check container status: " + err.Error()}, err
 	}
 
 	if len(result.Services) == 0 {
-		return Response{StatusCode: 404, Body: "Service not found"}
+		return Response{StatusCode: 404, Body: "Service not found"}, nil
 	}
 
 	srv := result.Services[0]
 	if srv.RunningCount == 1 {
-		return Response{
-			StatusCode: 200,
-			Body:       "Container is running",
-		}
+		return Response{StatusCode: 200, Body: "Container is running"}, nil
 	} else if srv.RunningCount == 0 {
-		return Response{
-			StatusCode: 200,
-			Body:       "Container is not running",
-		}
+		return Response{StatusCode: 200, Body: "Container is not running"}, nil
 	} else if srv.RunningCount > 1 {
-		return Response{
-			StatusCode: 500,
-			Body:       "Multiple containers are running",
-		}
+		return Response{StatusCode: 500, Body: "Multiple containers are running"}, nil
 	}
 
-	return Response{
-		StatusCode: 500,
-		Body:       "Internal server error",
-	}
+	return Response{StatusCode: 500, Body: "Internal server error"}, nil
 }
 
 func main() {
