4f03e2416013dc483fbfef30bc5ef67d6e81b76a
Karol Koś <kosiyyu@gmail.com>
2025-03-17 21:56:38 +0100

    Local build changes, build fixes, add icon, update .gitignore

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import Main from './screens/Main';
 import Settings from './screens/Settings';
@@ -9,25 +11,29 @@ const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Main"
-        screenOptions={{
-          headerShown: false,
-          cardStyle: { backgroundColor: 'transparent' },
-          cardStyleInterpolator: () => ({
-            cardStyle: {
-              opacity: 1,
-            },
-          }),
-          cardOverlayEnabled: false,
-          presentation: 'transparentModal',
-        }}
-      >
-        <Stack.Screen name="Main" component={Main} />
-        <Stack.Screen name="Settings" component={Settings} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Main"
+            screenOptions={{
+              headerShown: false,
+              cardStyle: { backgroundColor: 'transparent' },
+              cardStyleInterpolator: () => ({
+                cardStyle: {
+                  opacity: 1,
+                },
+              }),
+              cardOverlayEnabled: false,
+              presentation: 'transparentModal',
+            }}
+          >
+            <Stack.Screen name="Main" component={Main} />
+            <Stack.Screen name="Settings" component={Settings} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
 
